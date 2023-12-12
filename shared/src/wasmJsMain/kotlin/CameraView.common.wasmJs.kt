b64a37da6f9b043d829b2ac7ab3bb0b02123fb9b
Abdul Basit <abdulbasit5361234@gmail.com>
2023-12-12 10:54:04 +0500

    add video tag and source

--- shared/src/wasmJsMain/kotlin/CameraView.common.wasmJs.kt
+++ shared/src/wasmJsMain/kotlin/CameraView.common.wasmJs.kt
@@ -1,8 +1,24 @@
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import kotlinx.browser.document
+import kotlinx.browser.window
+import org.w3c.dom.HTMLVideoElement
+import org.w3c.dom.mediacapture.MediaStreamConstraints
 
 @Composable
 actual fun CameraView(modifier: Modifier) {
+    val videoElement = document.createElement("video") as HTMLVideoElement
+    videoElement.width = window.innerWidth
+    videoElement.translate=true
+    (window.navigator.getUserMedia(
+        MediaStreamConstraints(video = true.toJsBoolean(), audio = false.toJsBoolean()),
+        successCallback = {
+            videoElement.srcObject = it
+            videoElement.play()
+        },
+        errorCallback = {
+            println("Issue is $it")
+        }))
 
-
+    document.getElementById("fullScreenElement")!!.replaceWith(videoElement)
 }
--- webApp/src/wasmJsMain/kotlin/Main.wasm.kt
+++ webApp/src/wasmJsMain/kotlin/Main.wasm.kt
@@ -1,16 +1,16 @@
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.CanvasBasedWindow
+import org.w3c.dom.Text
 
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     CanvasBasedWindow("Transparent UI KMP Web") {
-        Box(modifier = Modifier.size(100.dp).background(Color.Red)) { }
+        App()
     }
 }
